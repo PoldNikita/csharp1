289b81a984134fb8d33e6168b7c33f55bfc0cba3
nikita <Nikita@example.com>
2022-11-24 23:56:57 +0300

    вторая задача сделана

---
+++ lesson_1/2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+if (num1 < num2)
+{
+    num1 = num2;
+}
+if (num1 < num3)
+{
+    num1 = num3;
+}
+else
+Console.Write($"Число {num1} наибольшее");
