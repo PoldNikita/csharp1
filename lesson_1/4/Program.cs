5719babf137f16bf82e7bf0d2a92516f48b7df6a
nikita <Nikita@example.com>
2022-11-25 00:20:45 +0300

    четвертая задача решена

---
+++ lesson_1/4/Program.cs
@@ -0,0 +1,13 @@
+﻿// программа, которая на вход принимает число (N),
+//    а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+
+int num1 = int.Parse(Console.ReadLine());
+int i = 2;
+
+while (i <= num1)
+{    
+    Console.Write($"{i} ");
+    i += 2;
+}
