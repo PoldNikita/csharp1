6b1f251f152c4a299f96dd8a77ceff5f7980bb35
nikita <Nikita@example.com>
2022-11-24 19:34:05 +0300

    создан код по находждению максимального числа

---
+++ lesson_1/1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите первое число:");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("введите второе число:");
+int num2 = int.Parse(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine($"первое введенное число (вы ввели: {num1}) больше второго");
+}
+else 
+Console.WriteLine($"второе введенное число (вы ввели: {num2}) больше первого");
