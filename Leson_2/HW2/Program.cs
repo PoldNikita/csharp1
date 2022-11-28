0508c8c47945c8c7e856db9261abca23108106ad
nikita <Nikita@example.com>
2022-11-28 21:32:46 +0300

    запушил 2й урок

---
+++ Leson_2/HW2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int digitsCount = (int)Math.Log10(number) + 1;
+int multiplier = (int)Math.Pow(10, digitsCount - 2); 
+int result = number % multiplier + number / multiplier / 10 * multiplier; 
+Console.WriteLine(result); 
---
+++ Leson_2/HW4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp3 
+{ 
+    internal class Program 
+    { 
+        static void Main(string[] args) 
+        { 
+            Console.Write(" Узнаем, является ли этот день выходным или нет?  "); 
+            while (true) 
+            { 
+                Console.Write("\n Введите номер дня недели от 1 до 7  "); 
+                int num = int.Parse(Console.ReadLine()); 
+                if (num >= 1 && num <= 5) 
+                { 
+                    Console.Write(" Нет, это не выходной"); 
+                } 
+ 
+                else if (num >= 6 && num <= 7) 
+                { 
+                    Console.Write(" Да, это выходной"); 
+                } 
+                else Console.Write(" Не существует такого дня недели"); 
+            } 
+        } 
+    } 
+}
