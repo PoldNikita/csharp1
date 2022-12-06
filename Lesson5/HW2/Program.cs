39515e9421e2ab3038cdf4a7d3dae430510ef505
nikita <Nikita@example.com>
2022-12-06 19:17:41 +0300

    исправил название переменной num_1

---
+++ Lesson5/HW2/Program.cs
@@ -0,0 +1,32 @@
+﻿// принимает на вход число и выдаёт сумму цифр в числе
+int num_1 = ReadInt("Введите число (минимум двузначное): ");
+int len = NumLen(num_1);
+SumNumbers(num_1, len);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NumLen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
