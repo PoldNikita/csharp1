14820ddbc71510929200375242afafa91ec8befd
nikita <Nikita@example.com>
2022-12-06 19:13:09 +0300

    ghj

---
+++ Lesson5/HW1/Program.cs
@@ -0,0 +1,19 @@
+﻿// цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень
+int num_A = ReadInt("Введите число A: ");
+int numB = ReadInt("Введите число B: ");
+ToDegree(num_A, numB);
+
+void ToDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
