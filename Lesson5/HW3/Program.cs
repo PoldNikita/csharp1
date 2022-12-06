f403d9fee628c17c4e96f162ef610ff94126a0d4
nikita <Nikita@example.com>
2022-12-06 19:05:51 +0300

    сделал 3 домашки

---
+++ Lesson5/HW3/Program.cs
@@ -0,0 +1,22 @@
+﻿void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] Massive(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(1, 100);
+    return arr;
+}
+
+int[] arr_1 = Massive(int.Parse(Console.ReadLine()));
+Print(arr_1);
+int[] arr_2 = Massive(int.Parse(Console.ReadLine()));
+Print(arr_2);
